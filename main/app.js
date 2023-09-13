11b0dd8d206be0aa22b75ff864bc5976ff1f7c2f
Alexis Reyna <alexisthecreator@Alexiss-MacBook-Pro.local>
2023-09-13 14:46:00 -0600

    Que onda

---
+++ main/app.js
@@ -0,0 +1,12 @@
+// I really don't know what am I going to code in this file. 
+// It's Wednesday 13 of September and I'm smoking while I'm on the computer.
+// So this is the result.
+//
+// With love. To Alison.
+//
+
+let usuarios = {
+    nombre: " ",
+    apellido: " ",
+    edad: " ",   
+}
